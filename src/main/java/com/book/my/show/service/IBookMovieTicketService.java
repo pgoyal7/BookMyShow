e216bb7bd0db82efdac8d8301bfe34ba613bf469
Piyush Goyal <pg.wiprotech@gmail.com>
2021-05-26 12:40:37 +0530

    Updated contract to cater book ticket related changes

--- src/main/java/com/book/my/show/service/IBookMovieTicketService.java
+++
@@ -1,8 +0,0 @@
-package com.book.my.show.service;
-
-import com.book.my.show.request.BookMovieTicketRequest;
-import com.book.my.show.response.BookMovieTicketResponse;
-
-public interface IBookMovieTicketService {
-    BookMovieTicketResponse bookMovieTicket(BookMovieTicketRequest bookMovieTicketRequest) throws Exception;
-}
