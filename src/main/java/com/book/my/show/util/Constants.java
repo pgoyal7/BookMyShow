7712d2fe41cfa258c2a008230cb729d52caaf8c8
Piyush Goyal <pg.wiprotech@gmail.com>
2021-05-26 14:54:04 +0530

    Updated sql to cater scheduler related changes

--- src/main/java/com/book/my/show/util/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.book.my.show.util;
-
-import java.time.format.DateTimeFormatter;
-
-public interface Constants {
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-}
